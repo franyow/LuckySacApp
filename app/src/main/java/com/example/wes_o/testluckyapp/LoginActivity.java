862e4e6b7d7c539f69b707d1a57d2c7e89481083
Franyo <franyow@gmail.com>
2018-04-05 19:38:27 -0500

    agregado helper para la bd sqlite

--- app/src/main/java/com/example/wes_o/testluckyapp/LoginActivity.java
+++ app/src/main/java/com/example/wes_o/testluckyapp/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.wes_o.testluckyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToMarkets(View view){
+        Intent i = new Intent(this,MarketsActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/wes_o/testluckyapp/MarketsActivity.java
+++ app/src/main/java/com/example/wes_o/testluckyapp/MarketsActivity.java
@@ -1,13 +1,33 @@
 package com.example.wes_o.testluckyapp;
 
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import OpenHelper.SQLite_OpenHelper;
+
 public class MarketsActivity extends AppCompatActivity {
+
+    SQLite_OpenHelper helper = new SQLite_OpenHelper(this,"MarketDB",null,1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_markets);
+
+        SetDatosPredefinidos();
+
+
     }
+
+    public void SetDatosPredefinidos(){
+
+        helper.openDB();
+        helper.insertarReg("Wong","Av. Sta. Cruz 771", "Miraflores ","-12.0960258","-77.02547370000002");
+        helper.insertarReg("Metro","Tiendas 290, 15047", "Surquillo","-12.1038253","-77.02055239999998");
+        helper.insertarReg("Tottus","Calle Las Begonias 785", "San Isidro","-12.0960258","-77.02547370000002");
+        helper.close();
+    }
+
+
 }
